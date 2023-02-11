0f18d140212b0d2f420afac744d701d325182624
Tom5521 <88908582+Tom5521@users.noreply.github.com>
2023-02-11 10:22:07 -0300

    Actualizacion a como lo tengo ahora :v

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # i3-config
 mi configuracion de i3 
+Los scripts los saque de la configuracion de endeavour os asique xd
---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cp -r i3status ~/.config/
+mkdir ~/.config/i3
+cp -r scripts ~/.config/i3
+cp i3blocks.conf ~/.config/i3
+cp config ~/.config/i3
